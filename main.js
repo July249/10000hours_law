fe7dc4bacf7aa5b03fb2c7936ed8a0e0f65c7603
jihyunglee <jhlee7125@gmail.com>
2022-10-27 22:20:15 +0900

    [add] (1) DOM 변수를 할당함에 있어 document에서 찾는 것을 최소화 / (2) 입력란의 value가 false가 되는 경우 border에 빨간색을 줘서 사용자에게 해당 입력란에 에러가 있음을 가시적으로 알림 / (3) 정규표현식을 이용하여 유효성을 평가하는 validationTest함수를 생성하였음 / (4) openModal, closeModal의 동작 방식에 있어 다양한 방법을 작성하였음 / (5) UI적 개선을 위하여 modal이 아닌 곳에서 발생한 이벤트가 window 전역 객체로 전달되는 경우 modal창을 닫도록 설정 / (6) url 복사하는 shareResult함수를 구현

--- main.js
+++ main.js
@@ -2,61 +2,138 @@
 /* Declare DOM Variables */
 
 /* Input Values */
-const fieldInputValue = document.getElementById('field-input');
-const timeInputValue = document.getElementById('time-input');
+const inputArea = document.querySelector('.insert-form');
+const fieldInputValue = inputArea.querySelector('#field-input');
+const timeInputValue = inputArea.querySelector('#time-input');
 
 /* Output Values */
-let fieldOutputValue = document.getElementById('field-output');
-let timeOutputValue = document.getElementById('time-output');
+const resultArea = document.querySelector('.result-sec');
+const fieldOutputValue = resultArea.querySelector('#field-output');
+const timeOutputValue = resultArea.querySelector('#time-output');
 
 /* buttons */
 const calculateBtn = document.querySelector('.btn-calc');
 const openModalBtn = document.querySelector('.btn-go');
 const closeModalBtn = document.querySelector('.btn-close');
-// const shareBtn = document.querySelector('.btn-share');
-
-/* Result */
-const result = document.querySelector('.result-sec');
+const shareBtn = document.querySelector('.btn-share');
 
 /* Modal */
 const modal = document.querySelector('.modal-screen');
 
-/* Define functions */
+/* Input Color Warning */
+if (!!fieldInputValue) {
+  fieldInputValue.style.border = 'none';
+} else {
+  fieldInputValue.style.border = '4px solid rgb(255, 73, 73)';
+}
+
+if (!!timeInputValue) {
+  timeInputValue.style.border = 'none';
+} else {
+  timeInputValue.style.border = '4px solid rgb(255, 73, 73)';
+}
+
+/* Functions */
+function validationTest(field, time) {
+  const strRegExp = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]|[A-Za-z]/g; // '0' ~ '9'가 아닌 문자(숫자가 아닌 문자)가 한 번 이상 반복되는 문자열을 전역 검사한다
+  const numRegExp = /[0-9]/g; // '0' ~ '9'가 한 번 이상 반복되는 문자열을 전역 검사한다
+  let inputValid = false;
+
+  if (!strRegExp.test(field) && !numRegExp.test(time)) {
+    alert('입력 사항을 적어 주세요.');
+    fieldInputValue.style.border = '4px solid rgb(255, 73, 73)';
+    timeInputValue.style.border = '4px solid rgb(255, 73, 73)';
+  } else if (!strRegExp.test(field)) {
+    alert('원하시는 전문 분야를 입력하여 주세요');
+    fieldInputValue.style.border = '4px solid rgb(255, 73, 73)';
+  } else if (!numRegExp.test(time)) {
+    alert('하루에 훈련하실 시간을 입력하여 주세요');
+    timeInputValue.style.border = '4px solid rgb(255, 73, 73)';
+  } else if (time > 24 && numRegExp.test(time)) {
+    alert('24시간 이내로 작성하여 주세요!');
+    timeInputValue.style.border = '4px solid rgb(255, 73, 73)';
+  } else {
+    inputValid = true;
+  }
+  // console.log(strRegExp.test(field));
+  // console.log(numRegExp.test(time));
+  // console.log(`input유효성결과:${inputValid}`);
+  return inputValid;
+}
+
 function calculateValues(e) {
   e.preventDefault();
-  if (!!fieldInputValue.value === false && !!timeInputValue.value === false) {
-    alert('입력 사항을 적어 주세요.');
-  } else if (!!fieldInputValue.value === false) {
-    alert('원하시는 전문 분야를 입력하여 주세요');
-  } else if (!!timeInputValue.value === false) {
-    alert('하루에 훈련하실 시간을 입력하여 주세요');
-  } else if (isNaN(Number(timeInputValue.value))) {
-    alert('1이상 24미만의 숫자를 입력하여 주세요.');
-  } else if (Number(timeInputValue.value) > 24) {
-    alert('24시간 이내로 작성하여 주세요!');
-  } else {
-    fieldOutputValue.value = fieldInputValue.value;
-    let trainingTimePerDay = Number(timeInputValue.value);
-    timeOutputValue.value = 10000 / trainingTimePerDay;
 
-    result.style.display = 'block';
+  const fieldInputTarget = fieldInputValue.value;
+  const timeInputTarget = timeInputValue.value;
+
+  let validInput = validationTest(fieldInputTarget, timeInputTarget);
+
+  if (validInput) {
+    let trainingTimePerDay = Number(timeInputValue.value);
+    timeOutputValue.textContent = Math.ceil(10000 / trainingTimePerDay);
+    fieldOutputValue.textContent = fieldInputValue.value;
+
+    resultArea.style.display = 'block';
   }
 
-  fieldOutputValue.innerText = fieldOutputValue.value;
-  timeOutputValue.innerText = timeOutputValue.value;
+  /*
+  // if (!!fieldInputValue.value === false && !!timeInputValue.value === false) {
+  //   alert('입력 사항을 적어 주세요.');
+  // } else if (!!fieldInputValue.value === false) {
+  //   alert('원하시는 전문 분야를 입력하여 주세요');
+  // } else if (!!timeInputValue.value === false) {
+  //   alert('하루에 훈련하실 시간을 입력하여 주세요');
+  // } else if (isNaN(Number(timeInputValue.value))) {
+  //   alert('1이상 24미만의 숫자를 입력하여 주세요.');
+  // } else if (Number(timeInputValue.value) > 24) {
+  //   alert('24시간 이내로 작성하여 주세요!');
+  // } else {
+  //   let trainingTimePerDay = Number(timeInputValue.value);
+  //   timeOutputValue.textContent = Math.ceil(10000 / trainingTimePerDay);
+  //   fieldOutputValue.textContent = fieldInputValue.value;
+
+  //   resultArea.style.display = 'block';
+  // }
+  */
+
+  console.log(fieldOutputValue.textContent);
+  console.log(timeOutputValue.textContent);
 }
 
 function openModal() {
-  modal.style.display = 'flex';
+  // For visibility property
+  // 1. Add class
+  modal.classList.add('open-modal');
+  // 2. Use toggle
+  // modal.classList.toggle('open-modal');
+  // For display property
+  // modal.style.display = 'flex';
 }
 
 function closeModal() {
-  modal.style.display = 'none';
+  // For visibility property
+  // 1. Remove class
+  modal.classList.remove('open-modal');
+  // 2. Use toggle
+  // modal.classList.toggle('open-modal');
+  // For display property
+  // modal.style.display = 'none';
 }
+// For UI
+window.addEventListener('click', (e) => {
+  e.target === modal ? modal.classList.remove('open-modal') : false;
+});
 
-// function shareResult() {}
+function shareResult() {
+  const url = window.location.href;
+
+  navigator.clipboard.writeText(url).then(() => {
+    alert('URL이 복사되었습니다');
+  });
+}
 
 calculateBtn.addEventListener('click', (e) => calculateValues(e));
 openModalBtn.addEventListener('click', openModal);
 closeModalBtn.addEventListener('click', closeModal);
-// shareBtn.addEventListener('click', shareResult);
+shareBtn.addEventListener('click', shareResult);
