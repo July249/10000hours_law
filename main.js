7210013183fd4213cde9769e7329cddf9b23a9e7
jihyunglee <jhlee7125@gmail.com>
2022-10-29 03:44:26 +0900

    [ADD] (1) field 정규표현식을 이용하여 전문분야 입력시 한글, 영문, 숫자만 가능하도록 함 / (2) time 정규표현식을 이용하여 1이상 24미만의 숫자만 입력이 가능하도록 함 / (3) 사용자가 input에 값을 입력할 때, 실시간으로 값에 대한 정규표현식 부합 여부를 판단하여 부적합한 경우 input border에 빨간색을 주어 사용자에게 입력사항에 오류가 있음을 바로 알림 / (4) validationAlert 함수의 if - else if - else 문에 alert() 이후 realTimeCheck 함수를 추가하여 alert 확인 이후 어느 input 부분에 오류가 있었는지 input border에 빨간색으로 오류가 있음을 사용자에게 즉각적으로 알림

--- main.js
+++ main.js
@@ -20,45 +20,62 @@ const shareBtn = document.querySelector('.btn-share');
 /* Modal */
 const modal = document.querySelector('.modal-screen');
 
-/* Input Color Warning */
-if (!!fieldInputValue) {
-  fieldInputValue.style.border = 'none';
-} else {
-  fieldInputValue.style.border = '4px solid rgb(255, 73, 73)';
-}
+/* RegExp */
+const fieldRegExp = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/;
+const timeRegExp = /^([1-9]|[01][0-9]|2[0-3])$/;
 
-if (!!timeInputValue) {
-  timeInputValue.style.border = 'none';
-} else {
-  timeInputValue.style.border = '4px solid rgb(255, 73, 73)';
-}
+/* Realtime Validation Check - Input Color Warning */
+// initial value setting
+let fieldValidResult;
+let timeValidResult;
+
+// 사용자가 input에 입력사항을 적을 때 정규표현식을 위반하는지 체크하여
+// 위반시 border를 빨간색으로 표시하여 입력사항에 주의를 요할 것을 사용자에게 알림
+fieldInputValue.addEventListener('keyup', () => {
+  fieldValidResult = strRegExp.test(fieldInputValue.value);
+
+  realTimeCheck(fieldInputValue, fieldValidResult);
+});
+
+timeInputValue.addEventListener('keyup', () => {
+  timeValidResult = numRegExp.test(timeInputValue.value);
+
+  realTimeCheck(timeInputValue, timeValidResult);
+});
 
 /* Functions */
-function validationTest(field, time) {
-  const strRegExp = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]|[A-Za-z]/g; // '0' ~ '9'가 아닌 문자(숫자가 아닌 문자)가 한 번 이상 반복되는 문자열을 전역 검사한다
-  const numRegExp = /[0-9]/g; // '0' ~ '9'가 한 번 이상 반복되는 문자열을 전역 검사한다
-  let inputValid = false;
-
-  if (!strRegExp.test(field) && !numRegExp.test(time)) {
-    alert('입력 사항을 적어 주세요.');
-    fieldInputValue.style.border = '4px solid rgb(255, 73, 73)';
-    timeInputValue.style.border = '4px solid rgb(255, 73, 73)';
-  } else if (!strRegExp.test(field)) {
-    alert('원하시는 전문 분야를 입력하여 주세요');
-    fieldInputValue.style.border = '4px solid rgb(255, 73, 73)';
-  } else if (!numRegExp.test(time)) {
-    alert('하루에 훈련하실 시간을 입력하여 주세요');
-    timeInputValue.style.border = '4px solid rgb(255, 73, 73)';
-  } else if (time > 24 && numRegExp.test(time)) {
-    alert('24시간 이내로 작성하여 주세요!');
-    timeInputValue.style.border = '4px solid rgb(255, 73, 73)';
+function realTimeCheck(inputNode, validResult) {
+  if (validResult) {
+    inputNode.style.border = 'none';
   } else {
-    inputValid = true;
+    inputNode.style.border = '4px solid rgb(255, 73, 73)';
   }
-  // console.log(strRegExp.test(field));
-  // console.log(numRegExp.test(time));
-  // console.log(`input유효성결과:${inputValid}`);
-  return inputValid;
+}
+
+function validationAlert(field, time) {
+  let validTestResult = false;
+
+  fieldValidResult = strRegExp.test(field);
+  timeValidResult = numRegExp.test(time);
+
+  if (!fieldValidResult && !timeValidResult) {
+    alert('입력 사항을 적어 주세요.');
+    realTimeCheck(fieldInputValue, fieldValidResult);
+    realTimeCheck(timeInputValue, timeValidResult);
+  } else if (!fieldValidResult) {
+    alert('원하시는 전문 분야를 입력하여 주세요');
+    realTimeCheck(fieldInputValue, fieldValidResult);
+  } else if (!timeValidResult) {
+    alert('하루에 훈련하실 시간을 입력하여 주세요');
+    realTimeCheck(timeInputValue, timeValidResult);
+  } else if (time > 24 && timeValidResult) {
+    alert('24시간 이내로 작성하여 주세요!');
+    realTimeCheck(timeInputValue, timeValidResult);
+  } else {
+    validTestResult = true;
+  }
+
+  return validTestResult;
 }
 
 function calculateValues(e) {
@@ -67,60 +84,26 @@ function calculateValues(e) {
   const fieldInputTarget = fieldInputValue.value;
   const timeInputTarget = timeInputValue.value;
 
-  let validInput = validationTest(fieldInputTarget, timeInputTarget);
+  let validInput = validationAlert(fieldInputTarget, timeInputTarget);
 
   if (validInput) {
-    let trainingTimePerDay = Number(timeInputValue.value);
+    let trainingTimePerDay = parseInt(timeInputValue.value);
+
     timeOutputValue.textContent = Math.ceil(10000 / trainingTimePerDay);
     fieldOutputValue.textContent = fieldInputValue.value;
 
     resultArea.style.display = 'block';
   }
-
-  /*
-  // if (!!fieldInputValue.value === false && !!timeInputValue.value === false) {
-  //   alert('입력 사항을 적어 주세요.');
-  // } else if (!!fieldInputValue.value === false) {
-  //   alert('원하시는 전문 분야를 입력하여 주세요');
-  // } else if (!!timeInputValue.value === false) {
-  //   alert('하루에 훈련하실 시간을 입력하여 주세요');
-  // } else if (isNaN(Number(timeInputValue.value))) {
-  //   alert('1이상 24미만의 숫자를 입력하여 주세요.');
-  // } else if (Number(timeInputValue.value) > 24) {
-  //   alert('24시간 이내로 작성하여 주세요!');
-  // } else {
-  //   let trainingTimePerDay = Number(timeInputValue.value);
-  //   timeOutputValue.textContent = Math.ceil(10000 / trainingTimePerDay);
-  //   fieldOutputValue.textContent = fieldInputValue.value;
-
-  //   resultArea.style.display = 'block';
-  // }
-  */
-
-  console.log(fieldOutputValue.textContent);
-  console.log(timeOutputValue.textContent);
 }
 
 function openModal() {
-  // For visibility property
-  // 1. Add class
   modal.classList.add('open-modal');
-  // 2. Use toggle
-  // modal.classList.toggle('open-modal');
-  // For display property
-  // modal.style.display = 'flex';
 }
 
 function closeModal() {
-  // For visibility property
-  // 1. Remove class
   modal.classList.remove('open-modal');
-  // 2. Use toggle
-  // modal.classList.toggle('open-modal');
-  // For display property
-  // modal.style.display = 'none';
 }
-// For UI
+
 window.addEventListener('click', (e) => {
   e.target === modal ? modal.classList.remove('open-modal') : false;
 });
