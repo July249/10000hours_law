b6817ea033770b42b513592434b140311ee3619f
jihyunglee <jhlee7125@gmail.com>
2022-10-31 16:33:18 +0900

    feat Modal창의 훈련하러가기 버튼 클릭시 Modal창이 닫히고 새창에서 인프런 1만시간 강좌 연결되도록 설정

--- main.js
+++ main.js
@@ -99,6 +99,10 @@ function openModal() {
 
 function closeModal() {
   modal.classList.remove('open-modal');
+  window.open(
+    'https://www.inflearn.com/course/1%EB%A7%8C%EC%8B%9C%EA%B0%84-%EC%9B%B9%ED%8E%98%EC%9D%B4%EC%A7%80-%EC%A0%9C%EC%9E%91',
+    '_blank'
+  );
 }
 
 window.addEventListener('click', (e) => {
