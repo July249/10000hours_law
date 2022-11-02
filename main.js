aae75233ed7b996f258af5e229e95ef13470e500
jihyunglee <jhlee7125@gmail.com>
2022-11-02 21:23:01 +0900

    fix 정규표현식의 평가 함수를 field와 time 별로 분리 및 함수 재사용성 향상 고려

--- main.js
+++ main.js
@@ -25,26 +25,34 @@ const fieldRegExp = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/;
 const timeRegExp = /^([1-9]|[01][0-9]|2[0-3])$/;
 
 /* Realtime Validation Check - Input Color Warning */
-// initial value setting
-let fieldValidResult;
-let timeValidResult;
-
 // 사용자가 input에 입력사항을 적을 때 정규표현식을 위반하는지 체크하여
 // 위반시 border를 빨간색으로 표시하여 입력사항에 주의를 요할 것을 사용자에게 알림
 fieldInputValue.addEventListener('keyup', () => {
-  fieldValidResult = fieldRegExp.test(fieldInputValue.value);
-
-  realTimeCheck(fieldInputValue, fieldValidResult);
+  const result = fieldValidationHandler(fieldInputValue.value);
+  errorStylingHandler(fieldInputValue, result);
 });
 
 timeInputValue.addEventListener('keyup', () => {
-  timeValidResult = timeRegExp.test(timeInputValue.value);
-
-  realTimeCheck(timeInputValue, timeValidResult);
+  const result = timeValidationHandler(timeInputValue.value);
+  errorStylingHandler(timeInputValue, result);
 });
 
 /* Functions */
-function realTimeCheck(inputNode, validResult) {
+function fieldValidationHandler(fieldInput) {
+  const fieldRegExp = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/;
+  const fieldValidTestResult = fieldRegExp.test(fieldInput);
+
+  return fieldValidTestResult;
+}
+
+function timeValidationHandler(timeInput) {
+  const timeRegExp = /^([1-9]|[01][0-9]|2[0-3])$/;
+  const timeValidTestResult = timeRegExp.test(timeInput);
+
+  return timeValidTestResult;
+}
+
+function errorStylingHandler(inputNode, validResult) {
   if (validResult) {
     inputNode.style.border = 'none';
   } else {
@@ -52,51 +60,38 @@ function realTimeCheck(inputNode, validResult) {
   }
 }
 
-function validationAlert(field, time) {
-  let validTestResult = false;
+function validationAlert(fieldValidResult, timeValidResult) {
+  let validCertification = false;
 
-  fieldValidResult = fieldRegExp.test(field);
-  timeValidResult = timeRegExp.test(time);
-
-  /* if (!!field === false && !!time === false) {
-    alert('입력 사항을 적어 주세요.');
-    realTimeCheck(fieldInputValue, fieldValidResult);
-    realTimeCheck(timeInputValue, timeValidResult);
-  } else if (!fieldValidResult) {
-    alert('원하시는 전문 분야를 확인하여 주세요.');
-    realTimeCheck(fieldInputValue, fieldValidResult);
-  } else if (!timeValidResult) {
-    alert('하루에 훈련하실 시간을 확인하여 주세요.');
-    realTimeCheck(timeInputValue, timeValidResult);
-  } else {
-    validTestResult = true;
-  } */
   if (!fieldValidResult && !timeValidResult) {
     alert('입력 사항을 적어 주세요.');
-    realTimeCheck(fieldInputValue, fieldValidResult);
-    realTimeCheck(timeInputValue, timeValidResult);
+    errorStylingHandler(fieldInputValue, fieldValidResult);
+    errorStylingHandler(timeInputValue, timeValidResult);
   } else if (!fieldValidResult) {
     alert('원하시는 전문 분야를 확인하여 주세요.');
-    realTimeCheck(fieldInputValue, fieldValidResult);
+    errorStylingHandler(fieldInputValue, fieldValidResult);
   } else if (!timeValidResult) {
     alert('하루에 훈련하실 시간을 확인하여 주세요.');
-    realTimeCheck(timeInputValue, timeValidResult);
+    errorStylingHandler(timeInputValue, timeValidResult);
   } else {
-    validTestResult = true;
+    validCertification = true;
   }
 
-  return validTestResult;
+  return validCertification;
 }
 
 function calculateValues(e) {
   e.preventDefault();
 
-  const fieldInputTarget = fieldInputValue.value;
-  const timeInputTarget = timeInputValue.value;
+  const fieldValidResult = fieldValidationHandler(fieldInputValue.value);
+  const timeValidResult = timeValidationHandler(timeInputValue.value);
 
-  let validInput = validationAlert(fieldInputTarget, timeInputTarget);
+  const validationTestResult = validationAlert(
+    fieldValidResult,
+    timeValidResult
+  );
 
-  if (validInput) {
+  if (validationTestResult) {
     let trainingTimePerDay = parseInt(timeInputValue.value);
 
     timeOutputValue.textContent = Math.ceil(10000 / trainingTimePerDay);
